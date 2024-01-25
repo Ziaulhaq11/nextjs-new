13a71d5b9c6b0fb172cbc3cd7d0572b8b5b728ba
Ziaulhaq11 <ziaullhaq11@gmail.com>
2024-01-25 20:16:02 +0530

    Done with SSR,CSR, Images etc

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,49 +1,62 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-
+import styles from "./navbar.module.css";
 const links = [
-    {
-        id : 1, 
-        title : "Home", 
-        url : "/"
-    },
-    {
-        id : 2, 
-        title : "Portfolio", 
-        url : "/portfolio"
-    },
-    {
-        id : 3, 
-        title : "Blog", 
-        url : "/blog"
-    },
-    {
-        id : 4, 
-        title : "About", 
-        url : "/about"
-    },
-    {
-        id : 5, 
-        title : "Contact", 
-        url : "/contact"
-    },
-    {
-        id : 6, 
-        title : "Dashboard", 
-        url : "/dashboard"
-    },
-]
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
-    return(
-        <div>
-            <Link href="/">Lamamia</Link>
-            <div>
-                 {links.map((link) => (
-                    <Link key={link.id} href={link.url}>{link.title}</Link>
-                 ))}
-            </div>
-        </div>
-    )
-}
-export default Navbar 
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Lamamia
+      </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        <button
+          onClick={() => {
+            console.log("Lougt");
+          }}
+          className={styles.logout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;

758d0518885eda4f05637514caad312f2fc1056d
unknown <ziaullhaq11@gmail.com>
2024-01-25 12:58:05 +0530

    project started

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <div>@2023 Lamami. All rights reserved</div>
+            <div></div>
+        </div>
+    )
+}
+export default Footer
